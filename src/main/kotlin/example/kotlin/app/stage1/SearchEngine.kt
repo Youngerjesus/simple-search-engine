8b8cc4aacb308bc143dbf137101d484ac3c05136
Youngerjesus <yjh930919@gmail.com>
2021-11-28 16:41:50 +0900

    add: SearchEngine Test: 주어진 문자열 리스트에서 찾고자하는 단어가 없는 경우

--- src/main/kotlin/example/kotlin/app/stage1/SearchEngine.kt
+++ src/main/kotlin/example/kotlin/app/stage1/SearchEngine.kt
@@ -1,4 +1,10 @@
 package example.kotlin.app.stage1
 
 class SearchEngine {
+    fun search(strings: List<String>, word: String): Int {
+        if (word in strings) {
+            return strings.indexOf(word)
+        }
+        return -1
+    }
 }
--- src/test/kotlin/example/kotlin/app/stage1/SearchEngineTest.kt
+++ src/test/kotlin/example/kotlin/app/stage1/SearchEngineTest.kt
@@ -1,5 +1,25 @@
 package example.kotlin.app.stage1
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
 
-internal class SearchEngineTest
+internal class SearchEngineTest {
+    private val searchEngine = SearchEngine()
+
+    @Test fun 주어진_문자열_리스트에서_찾고자하는_단어가_없는_경우() {
+        assertNotFound(
+            listOf("cat", "dog", "and", "mouse"),
+            "elephant"
+        )
+
+        assertNotFound(
+            listOf("one", "two", "three", "abcde", "abc"),
+            "abcd"
+        )
+    }
+
+    private fun assertNotFound(stringList: List<String>, word: String) {
+        val index: Int = searchEngine.search(stringList, word)
+        assertEquals(index, -1)
+    }
+}
