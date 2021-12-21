71dca29b1430b43fd3478c3af71d635d12a1d70a
Youngerjesus <yjh930919@gmail.com>
2021-12-22 02:41:57 +0900

    :recycle: refactoring

--- src/main/kotlin/example/kotlin/app/AppApplication.kt
+++ src/main/kotlin/example/kotlin/app/AppApplication.kt
@@ -1,11 +1,42 @@
 package example.kotlin.app
 
-import org.springframework.boot.autoconfigure.SpringBootApplication
+import example.kotlin.app.stage2.SearchEngine
+import example.kotlin.app.stage3.Menu
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
 class AppApplication
 
-fun main(args: Array<String>) {
-	runApplication<AppApplication>(*args)
+fun main() {
+	val (searchEngine, menu) = initialization()
+
+	while (true) {
+		menu.display()
+		val userInput: Menu.UserInput = menu.receiveUserInput()
+
+		if (userInput.choiceExit()) {
+			return
+		}
+
+		if (userInput.choiceFindPerson()) {
+			menu.printFoundPeople(searchEngine)
+		}
+
+		if (userInput.choicePrintAllPeople()) {
+			menu.printAllPeople(searchEngine)
+		}
+	}
 }
+
+fun initialization() : Pair<SearchEngine, Menu>{
+	println("Enter the number of people:")
+	val peopleCount = readLine()!!.toInt()
+
+	println("Enter all people:")
+	val searchEngine = SearchEngine()
+	for (i in 1..peopleCount) {
+		searchEngine.insert(readLine()!!)
+	}
+
+	return Pair(searchEngine, Menu())
+}
+
